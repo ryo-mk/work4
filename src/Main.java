b5f545cbb05a94e3a89b424170e792d193e31000
maki <mkr.fb-66@cameo.plala.or.jp>
2023-04-01 19:01:17 +0900

    toUpperCaseを追加

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> names = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida", "ootani");
+        List<String> result = names.stream()
+                .filter(name -> name.contains("a"))
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println(result);
     }
 }
